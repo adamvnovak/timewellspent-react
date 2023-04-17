1ec9548515b3b6d6dd046589c627dbbba17c1549
adamvnovak <adamvnovak@protonmail.com>
2023-04-17 16:12:34 -0700

    download

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LandingPage from './LandingPage';
 // import { Link } from "react-router-dom";
 import PrivacyPage from './PrivacyPage';
+import RedirectPage from './RedirectPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/download" element={<RedirectPage />} />
         </Routes>
       </Router>
     </>
---
+++ src/RedirectPage.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Link } from "react-router-dom";
+
+function RedirectPage() {
+  window.location.replace("https://apps.apple.com/app/apple-store/id6444199915");
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default RedirectPage;
